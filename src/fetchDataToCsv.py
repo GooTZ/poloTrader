f7669127b40f340aed200e51c81eeb11d1a9fccb
Domenik Weber <domenikweber@gmx.de>
2017-06-19 17:43:48 +0200

    Initial commit

---
+++ src/fetchDataToCsv.py
@@ -0,0 +1,65 @@
+import os
+import time
+import json
+import pandas as pd
+
+FETCH_URL = "https://poloniex.com/public?command=returnChartData&currencyPair=%s&start=%d&end=%d&period=300"
+PAIR_LIST = ["BTC_BTS", "BTC_ETH"]
+DATA_DIR = "newdata"
+COLUMNS = ["date","high","low","open","close","volume","quoteVolume","weightedAverage"]
+
+def get_data(pair):
+    datafile = os.path.join(DATA_DIR, pair+".csv")
+    timefile = os.path.join(DATA_DIR, pair)
+
+    print(datafile)
+
+    if os.path.exists(datafile):
+        newfile = False
+        start_time = int(open(timefile).readline()) + 1
+    else:
+        newfile = True
+        start_time = 1388534400     # 2014.01.01
+    end_time = 9999999999#start_time + 86400*30
+
+    url = FETCH_URL % (pair, start_time, end_time)
+    print("Get %s from %d to %d" % (pair, start_time, end_time))
+
+    df = pd.read_json(url, convert_dates=False)
+    print(df)
+
+    #import pdb;pdb.set_trace()
+
+    if df["date"].iloc[-1] == 0:
+        print("No data.")
+        return
+
+    end_time = df["date"].iloc[-1]
+    ft = open(timefile,"w")
+    ft.write("%d\n" % end_time)
+    ft.close()
+    outf = open(datafile, "a")
+    if newfile:
+        df.to_csv(outf, index=False, columns=COLUMNS)
+    else:
+        df.to_csv(outf, index=False, columns=COLUMNS, header=False)
+    outf.close()
+    print("Finish.")
+    time.sleep(30)
+
+
+def main():
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+
+    #df = pd.read_json("https://poloniex.com/public?command=return24hVolume")
+    #pairs = [pair for pair in df.columns if pair.startswith('BTC')]
+    pairs = PAIR_LIST
+    print(pairs)
+
+    for pair in pairs:
+        get_data(pair)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
