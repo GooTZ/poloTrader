085b35ffbf6ac493a4a08eebaf14a1ff5b73ed79
Domenik Weber <domenikweber@gmx.de>
2017-06-22 23:59:51 +0200

    added poloniex timing

--- app/util/DataProviders/LivetestingDataProvider.py
+++ app/util/DataProviders/LivetestingDataProvider.py
@@ -1,6 +1,7 @@
 import time
 import poloniex
 from app.util.DataProviders.DataProvider import DataProvider
+import app.util.Timing
 
 class LivetestingDataProvider(DataProvider):
 
@@ -8,42 +9,43 @@ class LivetestingDataProvider(DataProvider):
 		super().__init__(APIKey, Secret)
 
 	def doTheLoop(self):
-		# limit the calls to once a minute
+		# Place one order per second
+		# # limit the calls to once a minute
 		timeofThisLoop = time.monotonic()
-		delta1 = self.timeOfLastTickFetch - timeofThisLoop
-		if delta1 > -1:
+		delta = self.timeOfLastOrder - timeofThisLoop
+		if delta > -1:
+			return # next iteration if not enough time passed
+		self.timOfLastOrder = timeofThisLoop
+		self.placeOrder(orderQueue.popleft())
+
+		# limit the calls to once a minute
+		deltaMinute = self.timeOfLastTickFetch - timeofThisLoop
+		if deltaMinute > -(1 * 60):
 			return # next iteration if not enough time passed
 		self.timeOfLastTickFetch = timeofThisLoop
 
 		# TODO: api calls take pretty long, better timing maybe?
 		# Fetch all fast pased data from poloniex
-		self.polo.returnTicker()
-		self.polo.returnOrderBook()
+		ticker = self.polo.returnTicker()
+		orderbook = self.polo.returnOrderBook()
 		#self.polo.returnTradeHistory()
 		self.registeredFunctions['onData']()
 
-		#
-		orderQ = list()
 		if 'getOrderQueue' in self.registeredFunctions:
-			orderQ = self.registeredFunctions['getOrderQueue']()
-		for order in orderQ:
-			# TODO: get order infos and pass to function
-			placeOrder()
+			orderQueue = self.registeredFunctions['getOrderQueue']()
 
 		localtime = time.asctime( time.localtime(time.time()) )
 		print(localtime)
 
-		# execute this part of the loop only once every 5 minutes
-		# TODO: call only every specified tick
-		delta1 = self.timeOfLastCandlestickFetch - timeofThisLoop
-		if delta1 > -4.5:
+		# execute this part of the loop only once every n minutes
+		# where n is the specified self.timePeriod
+		delta5Min = self.timeOfLastCandlestickFetch - timeofThisLoop
+		if delta5Min > self.timePeriod:
 			return # next iteration if not enough time passed
 		self.timeOfLastCandlestickFetch = timeofThisLoop
 
 		# TODO: fetch for every pair or only specified ones?
 		# TODO: start = unixTimestamp(self.timeOfLastCandlestickFetch)
-		# TODO: set specified tick
-		#self.polo.returnChartData(self.pair, period = 300, start = start, end = 9999999999)
-
-	def placeOrder(self):
-		print("placing order")
+		start = app.util.Timing.getHistoricTimestamp(minute = self.timePeriod)
+		chart = self.polo.returnChartData("BTC_ETH", period = self.timePeriod, start = start, end = 9999999999)
+		print(chart)
