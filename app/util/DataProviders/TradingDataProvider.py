e65c29de46ff954ac8bc177ed88a88fe502c5964
Domenik Weber <domenikweber@gmx.de>
2017-06-29 15:53:52 +0200

    implemented live trading

--- app/util/DataProviders/TradingDataProvider.py
+++ app/util/DataProviders/TradingDataProvider.py
@@ -7,6 +7,58 @@ class TradingDataProvider(LivetestingDataProvider):
 	def __init__(self, APIKey, Secret, dataDir, dataQueue, orderQueue, instructionQueue):
 		super().__init__(APIKey, Secret, dataDir, dataQueue, orderQueue, instructionQueue)
 
+	def doTheLoop(self):
+		# Once a second print the fetching-data string
+		timeofThisLoop = time.monotonic()
+		delta = self.timeOfLastOrder - timeofThisLoop
+		if delta > -1:
+			return # next iteration if not enough time passed
+		self.timOfLastOrder = timeofThisLoop
+		print("Fetching data " + self.getSpinner(), end = "\r")
+
+		tickerData = self.getTickerData()
+		t = int(time.time())
+		for pair in tickerData:
+			self.appendToCandle(t, pair, tickerData[pair])
+
+		balances = self.polo.returnBalances()
+		self.dataQueue.put(("Balances", balances))
+
+		if not (self.orderQueue.empty()):
+			order = self.orderQueue.get()
+			self.placeOrder(order)
+
+		deltaMinute = self.timeOfLastTickFetch - timeofThisLoop
+		if deltaMinute > -60:
+			return # next iteration if not enough time passed
+		self.timeOfLastTickFetch = timeofThisLoop
+
+		print("appending data1")
+		self.appendCandlesToData()
+		self.currentCandles = {}
+
+	def appendCandlesToData(self):
+		outrDict = {}
+		for pair in self.currentCandles:
+			candle = self.currentCandles[pair]
+			print(candle['average'])
+			dataDict = {'date': candle['date'], 'low': candle['low'], 'open': candle['open'], 'average': candle['average'] / self.secondsPassed,
+			'close': candle['close'], 'high': candle['high'], 'volume': candle['volume'] / self.secondsPassed, 'isFrozen': candle['isFrozen']}
+			outrDict[pair] = dataDict
+		self.dataQueue.put(("Current", outrDict))
+		self.secondsPassed = 0
+
 	def placeOrder(self, order):
-		# TODO: implement actual ordering
-		print("placing order: ", order)
+		amount = abs(order.amount)
+		rate = abs(order.rate)
+		orderNmbr = 0
+		if order.amount < 0:
+			if order.mode == OrderMode.NORMAL:
+				orderNmbr = self.polo.sell(order.pair, rate, amount)
+			else:
+				orderNmbr = self.polo.sell(order.pair, rate, amount, order.mode.value)
+		elif order.amount > 0:
+			if order.mode == OrderMode.NORMAL:
+				orderNmbr = self.polo.buy(order.pair, rate, amount)
+			else:
+				orderNmbr = self.polo.buy(order.pair, rate, amount, order.mode.value)
